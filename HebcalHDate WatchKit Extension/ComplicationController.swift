4793a60f98157d46540451890596688f1939c879
Michael Radwin <michael@radwin.org>
2021-08-17 16:07:46 -0700

    Some actual content

--- HebcalHDate WatchKit Extension/ComplicationController.swift
+++ HebcalHDate WatchKit Extension/ComplicationController.swift
@@ -7,14 +7,22 @@
 
 import ClockKit
 
-
 class ComplicationController: NSObject, CLKComplicationDataSource {
     
     // MARK: - Complication Configuration
 
     func getComplicationDescriptors(handler: @escaping ([CLKComplicationDescriptor]) -> Void) {
+        let mySupportedFamilies = [CLKComplicationFamily.modularSmall,
+                                   CLKComplicationFamily.circularSmall,
+                                   CLKComplicationFamily.utilitarianSmall,
+                                   CLKComplicationFamily.utilitarianSmallFlat,
+                                   CLKComplicationFamily.utilitarianLarge,
+                                   CLKComplicationFamily.graphicCorner,
+                                   CLKComplicationFamily.graphicCircular
+        ];
         let descriptors = [
-            CLKComplicationDescriptor(identifier: "complication", displayName: "HebcalHDate", supportedFamilies: CLKComplicationFamily.allCases)
+            CLKComplicationDescriptor(identifier: "complicationHdate", displayName: "Hebrew Date", supportedFamilies: mySupportedFamilies),
+            CLKComplicationDescriptor(identifier: "complicationParsha", displayName: "Torah Portion", supportedFamilies: mySupportedFamilies)
             // Multiple complication support can be added here with more descriptors
         ]
         
@@ -42,7 +50,7 @@ class ComplicationController: NSObject, CLKComplicationDataSource {
     
     func getCurrentTimelineEntry(for complication: CLKComplication, withHandler handler: @escaping (CLKComplicationTimelineEntry?) -> Void) {
         // Call the handler with the current timeline entry
-        handler(nil)
+        handler(createTimelineEntry(forComplication: complication, date: Date()))
     }
     
     func getTimelineEntries(for complication: CLKComplication, after date: Date, limit: Int, withHandler handler: @escaping ([CLKComplicationTimelineEntry]?) -> Void) {
@@ -55,5 +63,128 @@ class ComplicationController: NSObject, CLKComplicationDataSource {
     func getLocalizableSampleTemplate(for complication: CLKComplication, withHandler handler: @escaping (CLKComplicationTemplate?) -> Void) {
         // This method will be called once per supported complication, and the results will be cached
         handler(nil)
+    }
+
+    var cal2 = Calendar(identifier: Calendar.Identifier.hebrew)
+    
+    // Return a timeline entry for the specified complication and date.
+    private func createTimelineEntry(forComplication complication: CLKComplication, date: Date) -> CLKComplicationTimelineEntry {
+        
+        // Get the correct template based on the complication.
+        let template = createTemplate(forComplication: complication, date: date)
+        
+        // Use the template and date to create a timeline entry.
+        return CLKComplicationTimelineEntry(date: date, complicationTemplate: template)
+    }
+    
+    // Select the correct template based on the complication's family.
+    private func createTemplate(forComplication complication: CLKComplication, date: Date) -> CLKComplicationTemplate {
+        switch complication.family {
+        case .modularSmall:
+            return createModularSmallTemplate(forDate: date)
+        case .modularLarge:
+            fatalError("*** Unsupported Complication Family ***")
+        case .utilitarianSmall, .utilitarianSmallFlat:
+            return createUtilitarianSmallFlatTemplate(forDate: date)
+        case .utilitarianLarge:
+            return createUtilitarianLargeTemplate(forDate: date)
+        case .circularSmall:
+            return createCircularSmallTemplate(forDate: date)
+        case .graphicCorner:
+            return createGraphicCornerTemplate(forDate: date)
+        case .graphicCircular:
+            return createGraphicCircleTemplate(forDate: date)
+        case .extraLarge:
+            fatalError("*** Unsupported Complication Family ***")
+        case .graphicRectangular:
+            fatalError("*** Unsupported Complication Family ***")
+        case .graphicBezel:
+            fatalError("*** Unsupported Complication Family ***")
+        case .graphicExtraLarge:
+            fatalError("*** Unsupported Complication Family ***")
+        @unknown default:
+            fatalError("*** Unknown Complication Family ***")
+        }
+    }
+
+    private func getHebDateString(forDate date: Date) -> String {
+        let hebrewCalendar = Calendar(identifier: Calendar.Identifier.hebrew)
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateStyle = .medium
+        dateFormatter.timeStyle = .none
+        dateFormatter.calendar = hebrewCalendar
+        return dateFormatter.string(from: date)
+    }
+    
+    // Return a modular small template.
+    private func createModularSmallTemplate(forDate date: Date) -> CLKComplicationTemplate {
+        let hebDateStr = getHebDateString(forDate: date)
+        let parts = hebDateStr.split(separator: " ")
+        // Create the data providers.
+        let dayNumberProvider = CLKSimpleTextProvider(text: String(parts[0]))
+        let monthNameProvider = CLKSimpleTextProvider(text: String(parts[1]))
+        
+        // Create the template using the providers.
+        return CLKComplicationTemplateModularSmallStackText(line1TextProvider: dayNumberProvider,
+                                                            line2TextProvider: monthNameProvider)
+    }
+    
+    // Return a utilitarian small flat template.
+    private func createUtilitarianSmallFlatTemplate(forDate date: Date) -> CLKComplicationTemplate {
+        // Create the data providers.
+        let hebDateStr = getHebDateString(forDate: date)
+        let parts = hebDateStr.split(separator: " ")
+        let dayNumberProvider = CLKSimpleTextProvider(text: String(parts[0]))
+        let monthNameProvider = CLKSimpleTextProvider(text: String(parts[1]))
+        let combinedProvider = CLKTextProvider(format: "%@ %@", dayNumberProvider, monthNameProvider)
+
+        // Create the template using the providers.
+        return CLKComplicationTemplateUtilitarianSmallFlat(textProvider: combinedProvider)
+    }
+    
+    // Return a utilitarian large template.
+    private func createUtilitarianLargeTemplate(forDate date: Date) -> CLKComplicationTemplate {
+        // Create the data providers.
+        let hebDateStr = getHebDateString(forDate: date)
+        let hebDateProvider = CLKSimpleTextProvider(text: hebDateStr)
+        // Create the template using the providers.
+        return CLKComplicationTemplateUtilitarianLargeFlat(textProvider: hebDateProvider)
+    }
+    
+    // Return a circular small template.
+    private func createCircularSmallTemplate(forDate date: Date) -> CLKComplicationTemplate {
+        // Create the data providers.
+        let hebDateStr = getHebDateString(forDate: date)
+        let parts = hebDateStr.split(separator: " ")
+        // Create the data providers.
+        let dayNumberProvider = CLKSimpleTextProvider(text: String(parts[0]))
+        let monthNameProvider = CLKSimpleTextProvider(text: String(parts[1]))
+
+        // Create the template using the providers.
+        return CLKComplicationTemplateCircularSmallStackText(line1TextProvider: dayNumberProvider,
+                                                             line2TextProvider: monthNameProvider)
+    }
+        
+    // Return a graphic template that fills the corner of the watch face.
+    private func createGraphicCornerTemplate(forDate date: Date) -> CLKComplicationTemplate {
+        // Create the data providers.
+        let hebDateStr = getHebDateString(forDate: date)
+        let hebDateProvider = CLKSimpleTextProvider(text: hebDateStr)
+        let labelProvider = CLKSimpleTextProvider(text: "Today")
+        // Create the template using the providers.
+        return CLKComplicationTemplateGraphicCornerStackText(innerTextProvider: hebDateProvider,
+                                                             outerTextProvider: labelProvider)
+    }
+    
+    // Return a graphic circle template.
+    private func createGraphicCircleTemplate(forDate date: Date) -> CLKComplicationTemplate {
+        // Create the data providers.
+        let hebDateStr = getHebDateString(forDate: date)
+        let parts = hebDateStr.split(separator: " ")
+        let dayNumberProvider = CLKSimpleTextProvider(text: String(parts[0]))
+        let monthNameProvider = CLKSimpleTextProvider(text: String(parts[1]))
+        // Create the template using the providers.
+        return CLKComplicationTemplateGraphicCircularStackText(line1TextProvider: dayNumberProvider,
+                                                               line2TextProvider: monthNameProvider)
     }
 }
--- HebcalHDate WatchKit Extension/ContentView.swift
+++ HebcalHDate WatchKit Extension/ContentView.swift
@@ -9,8 +9,27 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
-            .padding()
+        /*
+        let cal2 = Calendar(identifier: Calendar.Identifier.hebrew)
+
+        let now: Date = Date()
+
+        let dateFormatter2 = DateFormatter()
+        dateFormatter2.dateStyle = .medium
+        dateFormatter2.timeStyle = .none
+        dateFormatter2.calendar = cal2
+        let dateString2 = dateFormatter2.string(from: now)
+ */
+        VStack {
+            Text("Hebcal")
+                .font(.title)
+                .fontWeight(.thin)
+                .padding()
+            Text("Jewish holiday calendars & Hebrew date converter")
+                .fontWeight(.thin)
+                .multilineTextAlignment(.center)
+                .padding()
+        }
     }
 }
 
